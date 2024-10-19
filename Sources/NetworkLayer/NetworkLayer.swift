b20878765c44710bbb9adad78bddcc76e892b439
Atta Xgrid <atta@mediamindmail.com>
2024-10-19 14:36:51 +0200

    Added functionality for raw data download from server

--- Sources/NetworkLayer/NetworkLayer.swift
+++ Sources/NetworkLayer/NetworkLayer.swift
@@ -5,6 +5,7 @@ import Foundation
 @available(iOS 15.0.0, *)
 public protocol NetworkServiceProvider {
     func execute<T: Decodable>(request: Request) async throws -> T
+    func downloadData(request: Request) async throws -> Data
 }
 
 @available(iOS 15.0, *)
@@ -31,16 +32,23 @@ extension NetworkService: NetworkServiceProvider {
 
     public func execute<T: Decodable>(request: Request) async throws -> T {
         let (data, response) = try await session.data(for: request.urlRequest())
+        return try request.decoder.decode(T.self, from: data)
+    }
 
+    public func downloadData(request: Request) async throws -> Data {
+        return try await performRequest(request: request)
+    }
+
+    private func performRequest(request: Request) async throws -> Data {
+        let (data, response) = try await session.data(for: request.urlRequest())
         guard let httpResponse = response as? HTTPURLResponse else {
             throw NetworkServiceError.invalidResponse
         }
 
         // Validate status code
-        guard 200 ... 300 ~= httpResponse.statusCode else {
+        guard 200 ... 299 ~= httpResponse.statusCode else {
             throw NetworkServiceError.unacceptableStatusCode(httpResponse.statusCode)
         }
-
-        return try request.decoder.decode(T.self, from: data)
+        return data  // Return the raw data
     }
 }
