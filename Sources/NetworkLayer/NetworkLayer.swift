fdddcd69dafac7135945dc235d94c47be1bd41d0
Atta Xgrid <atta@mediamindmail.com>
2024-10-19 13:23:29 +0200

    Implement functionality of Async await with URLSession

--- Sources/NetworkLayer/NetworkLayer.swift
+++ Sources/NetworkLayer/NetworkLayer.swift
@@ -1,2 +1,46 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+
+@available(iOS 15.0.0, *)
+public protocol NetworkServiceProvider {
+    func execute<T: Decodable>(request: Request) async throws -> T
+}
+
+@available(iOS 15.0, *)
+public class NetworkService {
+    private let session: URLSession
+
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    // MARK: - Public API
+
+    deinit {
+        session.invalidateAndCancel()
+    }
+}
+
+@available(iOS 15.0, *)
+extension NetworkService: NetworkServiceProvider {
+    public enum NetworkServiceError: Error {
+        case invalidResponse
+        case unacceptableStatusCode(Int)
+    }
+
+    public func execute<T: Decodable>(request: Request) async throws -> T {
+        let (data, response) = try await session.data(for: request.urlRequest())
+
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkServiceError.invalidResponse
+        }
+
+        // Validate status code
+        guard 200 ... 300 ~= httpResponse.statusCode else {
+            throw NetworkServiceError.unacceptableStatusCode(httpResponse.statusCode)
+        }
+
+        return try request.decoder.decode(T.self, from: data)
+    }
+}
--- Sources/NetworkLayer/Request.swift
+++ Sources/NetworkLayer/Request.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+public struct Request {
+    public enum HttpMethod: String {
+        case get = "GET"
+        case post = "POST"
+        case put = "PUT"
+    }
+
+    public let scheme: String
+    public let baseURL: String
+    public let path: String
+    public let method: HttpMethod
+    public let queryItems: [URLQueryItem]
+    public let decoder: JSONDecoder
+
+    public init(
+        scheme: String = "https",
+        baseURL: String,
+        path: String,
+        method: HttpMethod = .get,
+        queryItems: [URLQueryItem] = [],
+        decoder: JSONDecoder = .init()
+    ) {
+        self.scheme = scheme
+        self.baseURL = baseURL
+        self.path = path
+        self.method = method
+        self.queryItems = queryItems
+        self.decoder = decoder
+    }
+
+    public func urlRequest() throws -> URLRequest {
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = baseURL
+        components.path = path
+        components.queryItems = queryItems.isEmpty ? nil : queryItems
+
+        guard let url = components.url else {
+            throw URLError(.badURL)
+        }
+
+        return URLRequest(url: url)
+    }
+}
